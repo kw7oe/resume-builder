59b242628bd3260163d457aa0b223463cd9e2a44
kw7oe <choongkwern@hotmail.com>
2022-04-03 14:40:56 +0800

    basic yaml as data -> handlebar -> html resume template

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use handlebars::Handlebars;
+use std::fs::File;
+
+fn main() {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_string("template", include_str!("resume.hbs"))
+        .unwrap();
+    let data: serde_json::Value = serde_yaml::from_str(include_str!("data.yml")).unwrap();
+
+    let mut output_file = File::create("target/table.html").unwrap();
+    handlebars
+        .render_to_write("template", &data, &mut output_file)
+        .unwrap();
+}
